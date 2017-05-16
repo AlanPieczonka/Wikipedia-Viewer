a0be2e90ae3377117f520e8517e66e154a373a7e
Alan <alanpieczonka@gmail.com>
2017-05-16 18:55:01 +0200

    JS and naming is done

--- js/script.js
+++ js/script.js
@@ -1,52 +1,55 @@
 console.log("JS file works!");
 
-const inp = document.getElementsByTagName('input')[0];
+let input = document.getElementsByTagName('input')[0];
 const buttonSearch = document.getElementsByClassName('button-search')[0];
 let inputValue = '';
 let html1 = '';
 let href = '';
+
 buttonSearch.addEventListener("click", function(){
-    
-    inputValue = document.getElementsByTagName('input')[0];
-    
-    console.log("inputValue = " + inputValue.value);
-    
-    search(inputValue.value);
-    inputValue.select();
-    
+        
+    search();
+ 
 });
 
-//enter button - the same function 
+input.addEventListener("keyup", function(e){
+    
+    if(e.keyCode == 13){
+            search();
+    }
+
+});
+
+    
 
 
-function search(searchValue){
-    
-    /*
-    var element = document.getElementById("element-id");
-element.parentNode.removeChild(element);
-*/
-    console.log("Find ");
-    console.log(searchValue);
-    
+function search(){ 
+    if($(".results").find("div")){
+        
+        console.log(".results div exists, remove it");
+        
+        $(".results").find("div").remove(); //prevents having a stack of divs 
+        ajaxCall(input.value);
+        input.select(); 
+        
+    }   
+};
+
+
+function ajaxCall(searchValue){  
         $.ajax({
         url: 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search='+ searchValue + '&limit=6&suggest=1&redirects=return&callback=ttt',
         jsonp: "callback",
         dataType: "jsonp",
         success: function( data ) {
             console.log(data);
-            console.log(data[1].length);
-            console.log(data[2].length);
-                
               for(let j=0; j<data[2].length; j++){
                   href = '<p><a href="' + data[3][j] + '">'+ data[3][j] +'</a></p>';
                   console.log("href " + href);
-                   html1  =  '<div>' + '<h2>' + data[1][j] + '</h2>' + '<p>' + data[2][j] + '</p>' + href +  '</div>';
+                  html1  =  '<div>' + '<h2>' + data[1][j] + '</h2>' + '<p>' + data[2][j] + '</p>' + href +  '</div>';
                   console.log(html1);
-                   $(".less").append(html1);
-                  
+                  $(".results").append(html1);
                 }
-            
-            
         },
         
         error: function(){
@@ -54,4 +57,4 @@ element.parentNode.removeChild(element);
     }
         
     });
-}
+};
